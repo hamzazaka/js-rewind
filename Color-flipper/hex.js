dc6db0fb23c703467b243214ad3924f7c4713fc7
Hamza Zaka <39979367+hamzazaka@users.noreply.github.com>
2022-04-07 16:14:46 -0400

    wokring on js color-flipper

---
+++ Color-flipper/hex.js
@@ -0,0 +1,20 @@
+const hex=[0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"];
+
+const btn =document.getElementById('btn');
+const color=document.querySelector('.color');
+
+btn.addEventListener('click',()=>{
+   let colorChange='#';
+   for(let i =0;i<6;i++){
+       let random=Math.floor(Math.random()* hex.length)
+       console.log(random);
+       colorChange+=hex[random]
+
+       document.body.style.backgroundColor=colorChange;
+       color.textContent=colorChange
+
+   }
+   console.log(colorChange);
+
+})
+
