e06a554fd74842acafb01f2bc7ab6c258ae055dc
Hamza Zaka <39979367+hamzazaka@users.noreply.github.com>
2022-04-16 17:59:56 -0400

    wokring on todo app

--- todoapp/app.js
+++ todoapp/app.js
@@ -1,6 +1,7 @@
 
 const form=document.getElementById('form');
 const input =document.getElementById('input');
+const todos=document.querySelector('.todos');
 
 form.addEventListener('submit', (e)=>{
     e.preventDefault();
@@ -8,6 +9,20 @@ form.addEventListener('submit', (e)=>{
     const todo=input.value;
 
     if(todo){
-        
+        const todoEl=document.createElement('li');
+        todoEl.innerText=todo;
+        todos.appendChild(todoEl);
+        input.value=''
+
+
+        todoEl.addEventListener('click',()=>{
+            todoEl.classList.toggle('completed')
+        })
+
+
+        todoEl.addEventListener('contextmenu',(e)=>{
+            e.preventDefault();
+            todoEl.remove()
+        })
     }
 })
